a320dec61b0a312684cf60a26c5880babf253b4b
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2022-11-13 22:33:03 -0600

    Added ai paddle need to fix ball hitting bottom screen

--- ball.lua
+++ ball.lua
@@ -35,6 +35,22 @@ function Ball:collide()
         local collisionPostion = middleBall - middlePlayer
         self.YVel = collisionPostion * 5
     end
+
+    if checkCollision(self, AiPlayer) then
+        self.XVel = -self.speed 
+        local middleBall = self.y + self.width / 2
+        local middleAiPlayer = AiPlayer.y + AiPlayer.height / 2
+        local collisionPostion = middleBall - middleAiPlayer
+        self.YVel = collisionPostion * 5
+    end
+
+    if self.y < 0 then
+      self.y = 0
+      self.YVel = -self.YVel
+    elseif self.y + self.height > love.graphics.getHeight() then
+        self.y = love.graphics.getHeight - self.height
+        self.YVel = -self.YVel
+    end
 end
 
 
--- main.lua
+++ main.lua
@@ -1,21 +1,25 @@
 require("player")
 require("Ball")
+require("aiplayer")
 
 
 function love.load()
     Player:load()
     Ball:load()
+    AiPlayer:load()
 end
 
 function love.update(dt)
     Player:update(dt)
     Ball:update(dt)
+    AiPlayer:update(dt)
 end
 
 
 function love.draw()
     Player:draw()
     Ball:draw()
+    AiPlayer:draw()
 end
 
 function checkCollision(a, b)
